f7085aacbf6857c368a56a96103d153a5a3a8058
Alexis <mazaudiera@gmail.com>
2020-05-17 21:22:12 +0200

    Removed capistrano deployment to use EasyCorp deploy method, much simpler

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source "https://rubygems.org"
-
-gem 'capistrano'
-gem 'capistrano-symfony'
-gem 'capistrano-composer'
-gem 'capistrano-copy-files'
-gem 'capistrano-pending'
-gem 'capistrano-lephare', github: 'le-phare/capistrano-lephare'
---
+++ config/preprod/deploy.php
@@ -0,0 +1,33 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+use EasyCorp\Bundle\EasyDeployBundle\Configuration\Option;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
+            ->server('u87073604@home655277423.1and1-data.host')
+            // the absolute path of the remote server directory where the project is deployed
+            ->deployDir('/kunden/homepages/39/d655277423/htdocs/izap4u')
+            // the URL of the Git repository where the project code is hosted
+            ->repositoryUrl('git@github.com:AlexiZ/izap4u.git')
+            // the repository branch to deploy
+            ->repositoryBranch('master')
+            ->keepReleases(3)
+            ->useSshAgentForwarding(true)
+            ->remoteComposerBinaryPath('php7.3-cli /kunden/homepages/39/d655277423/htdocs/izap4u/composer.phar')
+            ->remotePhpBinaryPath('php7.3-cli')
+//            ->composerInstallFlags('--prefer-dist --no-interaction --no-progress --optimize-autoloader --apcu-autoloader')
+            ->symfonyEnvironment('prod')
+        ;
+    }
+
+    public function beforePreparing()
+    {
+        $this->log('Remote composer dump autoload');
+        $this->runRemote(sprintf('%s dump-autoload', $this->getConfig(Option::remoteComposerBinaryPath)));
+    }
+};

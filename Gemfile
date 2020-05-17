0ca8796d8d58eec8fcec103c9c6601c9d927e0c1
Alexis <mazaudiera@gmail.com>
2020-05-17 19:20:17 +0200

    Installed capistrano and set up deployment environment

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'capistrano'
+gem 'capistrano-symfony'
+gem 'capistrano-composer'
+gem 'capistrano-copy-files'
+gem 'capistrano-pending'
+gem 'capistrano-lephare', github: 'le-phare/capistrano-lephare'
---
+++ config/capistrano/stages/production.rb
@@ -0,0 +1,15 @@
+set :stage, :production
+set :domain, 'home655277423.1and1-data.host'
+set :ssh_port, 22
+
+server fetch(:domain), user: 'u87073604', roles: %w{web app db}
+
+set :deploy_to, '/kunden/homepages/39/d655277423/htdocs/izap4u'
+set :branch, 'master'
+
+set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs'
+set :composer_php, 'php7.1-cli'
+
+SSHKit.config.command_map[:composer] = "#{fetch(:deploy_to)}/shared/composer.phar"
+SSHKit.config.command_map[:composer] = "#{shared_path.join("composer.phar")}"
+SSHKit.config.command_map[:php] = "php7.1-cli"
